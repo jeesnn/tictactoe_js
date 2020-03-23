1155419985430637ad9a01742ce2b1aa9f7ba3ab
zhenjess <jessicazhen8@gmail.com>
2020-03-23 09:49:51 -0700

    Prompt user to mark a cell position on board

--- game.js
+++ game.js
@@ -11,4 +11,20 @@ class Game {
     playMove(pos) {
         this.board.placeMark(pos, this.currentPlayer);
     }
+
+    promptMove(reader, callback) {
+        const game = this; //the game
+
+        this.board.print(); //print out the board
+        console.log(`Current Turn: ${this.currentPlayer}`); //current user's turn
+
+        //prompt user to enter/mark a position[row, col] cell
+        reader.question('Enter rowIdx: ', rowIdxStr => {
+            const rowIdx = parseInt(rowIdxStr);
+            reader.question('Enter colIdx: ', colIdxStr => {
+                const colIdx = parseInt(colIdxStr);
+                callback([rowIdx, colIdx]);
+            });
+        });
+    }
 }
