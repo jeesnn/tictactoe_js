8971085327828c4e2c871e421a1cfb956d0737d8
zhenjess <jessicazhen8@gmail.com>
2020-03-23 09:45:24 -0700

    Initialize board and player to start game

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
 //how to play tictactoe game
 const Board = require('./board');
 const MoveError = require('./moveError');
+
+class Game {
+    constructor() { //initialize board and player
+        this.board = new Board();
+        this.currentPlayer = Board.marks[0];
+    }
+}
