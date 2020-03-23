a5e2adfab50c521e3551bb0d3d1f52116cdb75d4
zhenjess <jessicazhen8@gmail.com>
2020-03-23 09:42:53 -0700

    Comment on Move error

--- moveError.js
+++ moveError.js
@@ -3,3 +3,7 @@ const MoveError = function (msg) {
 };
 
 module.exports = MoveError;
+
+//MoveError is a vanilla Object for now but should be an inheritance
+//MoveError really should  be a child class of the built in Error object provided
+//by JS
