5a9c1ca0c9f1f9167821d8a64516bc2a9e2029fc
zhenjess <jessicazhen8@gmail.com>
2020-03-23 09:12:03 -0700

    Validate if is a board position

--- board.js
+++ board.js
@@ -2,4 +2,14 @@ class Board {
     constructor() {
         this.grid = Board.makeGrid();
     }
+
+    isEmptyPos(pos) {
+        if (!Board.isValidPos(pos)) {
+            //if not a valid board position
+            throw new MoveError("Is not a valid position!");
+        } else {
+            //is a valid board position so set to empty not filled position
+            return (this.grid[pos[0]][pos[1]] === null);
+        }
+    }
 }
